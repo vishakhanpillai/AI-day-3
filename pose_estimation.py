bf9531f7846edb4406a6e8b9b2cfde94e0d01422
Vishakhan Pillai <pillaivishakhan2003@gmail.com>
2025-06-05 12:04:45 +0530

    modified post

--- pose_estimation.py
+++ pose_estimation.py
@@ -32,9 +32,9 @@ cap = cv2.VideoCapture(0)
 last_screenshot_time = 0
 cooldown_seconds = 10
 
-SENDER_EMAIL = 'vishakhanpillaidev@gmail.com'
-SENDER_PASSWORD = 'hycb dspl fury jzue'
-RECEIVER_EMAIL = 'pillaivishakhan@gmail.com'
+SENDER_EMAIL = '' #add your email
+SENDER_PASSWORD = '' #add your app password
+RECEIVER_EMAIL = '' #add receivers email
 
 while cap.isOpened():
     ret, frame = cap.read()
